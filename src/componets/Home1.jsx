28292265ab281890092315cb55428906e8dac376
Kazama319 <bhanupratap_co21a3_40@dtu.ac.in>
2024-07-07 20:14:29 +0530

    Update Home1.jsx

--- src/componets/Home1.jsx
+++ src/componets/Home1.jsx
@@ -18,12 +18,12 @@ function Home() {
                         </p>
                         <div className="flex justify-center space-x-4">
                             <Link to="/login">
-                                <Button bgColor="bg-indigo-600" textColor="text-white">
+                                <Button bgColor="bg-indigo-600" textColor="text-white" className="transition-transform duration-300 transform hover:scale-105 hover:bg-indigo-700">
                                     Login
                                 </Button>
                             </Link>
                             <Link to="/signup">
-                                <Button bgColor="bg-green-500" textColor="text-white">
+                                <Button bgColor="bg-green-600" textColor="text-white" className="transition-transform duration-300 transform hover:scale-105 hover:bg-green-700">
                                     Sign Up
                                 </Button>
                             </Link>
@@ -43,12 +43,12 @@ function Home() {
                         </p>
                         <div className="flex justify-center space-x-4">
                             <Link to="/features">
-                                <Button bgColor="bg-blue-600" textColor="text-white">
+                                <Button bgColor="bg-blue-600" textColor="text-white" className="transition-transform duration-300 transform hover:scale-105 hover:bg-blue-700">
                                     Features
                                 </Button>
                             </Link>
                             <Link to="/about">
-                                <Button bgColor="bg-yellow-500" textColor="text-gray-900">
+                                <Button bgColor="bg-red-500" textColor="text-white" className="transition-transform duration-300 transform hover:scale-105 hover:bg-red-600">
                                     About
                                 </Button>
                             </Link>
@@ -56,6 +56,13 @@ function Home() {
                     </div>
                 </Container>
             </section>
+            <div className="mt-8 flex justify-center">
+    <Link to="/contact">
+        <Button bgColor="bg-purple-600" textColor="text-white" className="transition-transform duration-300 transform hover:scale-105 hover:bg-purple-700">
+            Give Us Feedback
+        </Button>
+    </Link>
+</div>
 
             
         </div>
