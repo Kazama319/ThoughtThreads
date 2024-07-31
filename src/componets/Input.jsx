ab6607391c9ffa5e8a456619fc64f786d053e529
Kazama319 <you@example.combhanupratap_co21a3_40@dtu.ac.in>
2024-07-31 21:16:50 +0530

    label colour change

--- src/componets/Input.jsx
+++ src/componets/Input.jsx
@@ -10,14 +10,14 @@ const Input = React.forwardRef( function Input({
     return (
         <div className='w-full'>
             {label && <label 
-            className='inline-block mb-1 pl-1 text-black' 
+            className='inline-block mb-1 pl-1 text-blue-600' 
             htmlFor={id}>
                 {label}
             </label>
             }
             <input
             type={type}
-            className={`px-3 py-2 rounded-lg bg-white placeholder-slate-400   text-black outline-none focus:bg-gray-300 duration-200 border border-gray-200 w-full  ${className}`}
+            className={`px-3 py-2 rounded-lg bg-white placeholder-slate-600   text-black outline-none focus:bg-gray-300 duration-200 border border-gray-200 w-full  ${className}`}
             ref={ref}
             {...props}
             id={id}
